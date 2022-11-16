39657b2f8fdb44f3e89a39a3f8d84aaab7fed93b
David BRUNET <david.brunet@hotmail.fr>
2022-11-16 15:20:53 +0100

    Ajout d'un lien vers le repot Gihub

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import logo from "../assets/logo.png";
+import github from "../assets/github.svg";
 
 const Hero = () => (
   <div className="text-center hero my-5">
@@ -10,6 +11,9 @@ const Hero = () => (
       Connectez-vous pour consulter cette application.{" "}
     </p>{" "}
     <p> Découvrez <a href="https://www.figma.com/file/XV3IMdUHX0evggp32diLta/DemoStimuli?node-id=0%3A1&t=DLtUn17QCwQmpDrt-1" target="_blank" rel="noreferrer">la maquette sur Figma</a> et les avantages de la création de compte. </p>{" "}
+    <a href="https://github.com/DavidSimplon/demoStimuli" target="_blank" rel="noreferrer">
+      Code source Github <img src={github} alt='github'/>
+    </a>
   </div>
 );
 
