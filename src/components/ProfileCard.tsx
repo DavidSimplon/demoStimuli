d9f43a5abefb95bbfa24d612b7d06a630d63991f
David BRUNET <david.brunet@hotmail.fr>
2022-11-16 15:04:34 +0100

    Suppression des warning

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,11 +1,10 @@
-import { FunctionComponent, useState } from 'react'
+import { FunctionComponent } from 'react'
 import Grow from '@mui/material/Grow';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import { Grid } from '@mui/material';
 
 import Typography from '@mui/material/Typography';
