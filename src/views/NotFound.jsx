5557c273a67f57c3b27bcc63ab36b83a1e440cf8
David BRUNET <david.brunet@hotmail.fr>
2022-11-23 22:17:58 +0100

    Suppression du bundle.js

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 const NotFound = () => (
   <Fragment>
     <div className="text-center">
       <h1 >Page introuvable !</h1>
+      <div>L&apos;adresse &quot;{useLocation().pathname}&quot; n&apos;existe pas !</div>
       <Link to="/">{"Retourner à l'accueil"}</Link>
     </div>
   </Fragment>
